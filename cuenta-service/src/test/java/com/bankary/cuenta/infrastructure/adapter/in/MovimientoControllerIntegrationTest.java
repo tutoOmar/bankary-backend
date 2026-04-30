1696a6ca471b541f0f8d20f3cdc3898dd20d9dc0
tutoOmar <omar-ortiz@javeriana.edu.co>
2026-04-29 22:42:19 -0500

    fix: Import error

--- cuenta-service/src/test/java/com/bankary/cuenta/infrastructure/adapter/in/MovimientoControllerIntegrationTest.java
+++ cuenta-service/src/test/java/com/bankary/cuenta/infrastructure/adapter/in/MovimientoControllerIntegrationTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
